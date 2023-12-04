2d8af614f71738c460b11fb37e86dae3efc9c975
manojraut005 <102407303+manojraut005@users.noreply.github.com>
2023-12-04 17:39:38 +0530

    commit

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const BlogDetails = ({post}) => {
   return (
-    <div >
+    <div  >
       <NavLink to={`/blog/${post.id}`} >
         <span className="font-bold text-lg ">{post.title}</span>
       </NavLink>
@@ -17,7 +17,7 @@ const BlogDetails = ({post}) => {
       </p>
       <p className='text-sm mt-[4px]'> Posted on {post.date} </p>
       <p className='text-md mt-[14px]'> {post.content}</p>
-      <div className='flex gap-x-3'>
+      <div className='flex flex-wrap gap-x-3'>
         {post.tags.map( (tag, index) => (
             <NavLink key={index} to={`/tags/${tag.replaceAll(" ","-")}`}>
                 <span className="text-blue-700 underline font-bold text-xs mt-[5px]">{`#${tag}`}</span>
